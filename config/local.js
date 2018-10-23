4041a0cc4c1bff5266fefe69635410125d42dd3c
makhokit <makhorig@gmail.com>
2018-10-23 14:25:38 +0400

    Update Wallet Core

---
+++ config/local.js
@@ -0,0 +1,13 @@
+module.exports = {
+  gateways: {
+    logger: {
+      level: 'info',
+    },
+    iotexCore: {
+      serverUrl: 'http://159.89.223.147:14004/',
+    },
+    walletCore: {
+      serverUrl: '159.89.223.147:42124',
+    },
+  },
+};
