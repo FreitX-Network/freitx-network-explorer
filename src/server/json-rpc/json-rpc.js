c843a00ab5977190499a83170070e7dec1fd7f69
makhokit <makhorig@gmail.com>
2018-10-26 14:12:28 +0400

    Revert

--- src/server/json-rpc/json-rpc.js
+++ src/server/json-rpc/json-rpc.js
@@ -12,28 +12,4 @@ export function setJsonRpcRoutes(server: any) {
       name: 'JsonRpc',
     })
   );
-  server.post(
-    'api/wallet-core/',
-    'api/wallet-core/',
-    async ctx => {
-      const {jsonrpc, id, method, params} = ctx.request.body;
-      try {
-        const result = await server.gateways.walletCore[method](...params);
-        return ctx.response.body = {
-          id,
-          jsonrpc,
-          result,
-        };
-      } catch (e) {
-        return ctx.response.body = {
-          id,
-          jsonrpc,
-          error: {
-            code: -32600,
-            message: e.message,
-          },
-        };
-      }
-    }
-  );
 }
