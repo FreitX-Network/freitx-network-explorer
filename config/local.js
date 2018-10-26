dd4e9b4f6bb92c69cfcbc51408b29cc9e38513a8
makhokit <makhorig@gmail.com>
2018-10-26 14:54:03 +0400

    Connect Machine to Server

---
+++ config/local.js
@@ -0,0 +1,13 @@
+module.exports = {
+  gateways: {
+    logger: {
+      level: 'info',
+    },
+    iotexCore: {
+      serverUrl: 'http://176.57.71.37:14004/',
+    },
+    walletCore: {
+      serverUrl: '176.57.71.37:42124',
+    },
+  },
+};
